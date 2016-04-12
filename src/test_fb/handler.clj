d601bb9efef003e68721df50037f4f55d7d46985
gzeureka <gzeureka@gmail.com>
2016-04-12 21:54:20 +0800

    add handler

--- src/test_fb/handler.clj
+++ src/test_fb/handler.clj
@@ -1,2 +1,10 @@
 (ns test-fb.handler
-  )
+  (:require [ring.middleware.defaults :refer (wrap-defaults site-defaults secure-site-defaults)]
+            [ring.util.response :refer (not-found response)]
+            [taoensso.timbre :as log]
+            [clojure.string :refer (trim blank?)]
+            [clojure.java.io :as io]
+            ))
+
+(defn app [request]
+  (response "hello world"))
